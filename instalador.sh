0c4ce60d4dd31741c55a8c91894ace851b57b2f5
IngRamirezJezhuah <ramierzalvaradodavidjezhuah@gmail.com>
2026-04-23 19:04:54 -0600

    Update + fixes - agregando soporte Mulitdistro

--- instalador.sh
+++ instalador.sh
@@ -13,6 +13,7 @@
 verde='\033[0;32m'
 azul='\033[0;34m'
 amarillo='\033[1;33m'
+rojo='\033[1;31m'
 nc='\033[0m'
 
 #========================
@@ -51,12 +52,97 @@ echo -e "${azul}
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠷⠶⠶⢧⠶⡶⢶⠶⡷⢶⠾⡶⢶⠷⡶⠾⠼⢿⣠⣁⣐⣠⣀⣉⣒⣰⣂⣦⣥⣖⣴⠮⠿⠳⠶⠾⠴⠿⠿⠿⠳⠶⣦⣀⠀⠀⠀⠀⠀⠀⡀⠂
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣦⣶⣼⡟⠃⠀⠀⠀⠀⠀⠃⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠀${nc}"
-echo -e "${amarillo}                 instalador de PrettyBash"j 
+echo -e "${amarillo}                 instalador de PrettyBash" 
 echo -e "${verde}================================================================${nc}"
 
-#==============================
-#       VALIDACIONES          |
-#==============================
+#================================
+#       VALIDACIONES Y PERMISOS |
+#================================
 
 echo -e "${verde}[1/nose]${nc}"
+echo -e "${amarillo} Verificacion de autorizacion...${nc}"
 echo -ne "${azul}¿Deseas instalar las dependencias necesarias? (s/n): ${nc}"
+read -r respuesta
+sleep 1
+
+if [[ ! "$respuesta" =~ [SsyY]$ ]]; then
+  echo -e "${verde}Instalacion cancelada...${nc}"
+  echo -e "${amarillo}!Adios¡  Y(˶˃ ᵕ ˂˶)Y ${nc}"
+  exit 
+fi
+
+echo -e "${azul} Verificando Permisos...${nc}"
+
+if [ "$EUID" -ne 0 ]; then 
+  echo -e "${verde} Se requiere Privilegios avanzados, PorFavor, Ingresa tu contraseña: ${nc}"
+  sudo -v || exit 1
+fi
+sleep 1
+
+
+#=================================================
+#       Actualizacion eh instalacion
+#       Proximamente lo cocinare
+#=================================================
+
+
+
+#================================================
+#      instalador segun la distro
+#                 By Dj
+#================================================
+PAQUETES=(
+  "catimg"
+)
+
+esta_instalado() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+instalar_con_sudo() {
+  echo -e "[3/ ] ${amarillo}  Instalando $1 ... ${nc}"
+  sudo "$@"
+}
+
+echo -e "${azul} Iniciando instalacion de dependencias...${nc}"
+
+for pkg in "${PAQUETES[@]}"; do
+  if esta_instalado "$pkg"; then
+    echo -e "${verde} [✔] $pkg ya esta instalado ${nc}"
+  else 
+    if which apt >/dev/null 2>&1; then
+      # para los de devian y derivadas
+      instalar_con_sudo apt install -y "$pkg"
+    elif which pacman >/dev/null 2>&1; then
+      # para arch y derivadas
+      instalar_con_sudo pacman -S --noconfirm "$pkg"
+    elif which dnf >/dev/null 2>&1; then
+      # para fedora y derivadas
+      instalar_con_sudo dnf install -y "$pkg"
+    elif which yum >/dev/null 2>&1; then
+      # centos y variedad
+      instalar_con_sudo yum install -y  "$pkg"
+    elif which zypper >/dev/null 2>&1; then
+      # para opensuse y derivadas
+      instalar_con_sudo zypper install -y "$pkg"  
+    elif which emerge >/dev/null 2>&1; then
+      # para los de gentoo
+      instalar_con_sudo emerge --ask=n "$pkg"
+    else
+      echo -e "${rojo} No se encontro ningun gestor de paquetes compatibles en el PATH${nc}"
+    fi
+  fi
+done
+
+
+
+echo -e "${verde}==================================================${nc}"
+echo -e "${azul}        !Dependencias Instaladas¡ ${nc}"
+echo -e "${verde}==================================================${nc}"
+
+echo -e "${amarillo} Integrando a la terminal...${nc}"
+
+#source 
+echo -e "${azul}  $(dirname "$0")" 
+
+
